651fab04fe4a67ef3c9c14f6cbf63f727b112556
Romaine De La Cruz <r2dcruz@gmail.com>
2022-02-25 23:15:59 -0800

    chaining

--- structures/hash-tables.java
+++ structures/hash-tables.java
@@ -26,3 +26,105 @@ public abstract class HashTable {
    // not found.
    public abstract Object search(Object key);
 }
+
+class ChainingHashTableItem {
+   public Object key;
+   public Object value;
+   public ChainingHashTableItem next;
+
+   ChainingHashTableItem(Object itemKey, Object itemValue) {
+      key = itemKey;
+      value = itemValue;
+      next = null;
+   }
+}
+
+public class ChainingHashTable extends HashTable {
+   private ChainingHashTableItem[] table;
+
+   public ChainingHashTable(int initialCapacity) {
+      table = new ChainingHashTableItem[initialCapacity];
+   }
+   
+   public ChainingHashTable() {
+      // Initialize with an initial capacity of 11
+      this(11);
+   }
+
+   // Inserts the specified key/value pair. If the key already exists, the 
+   // corresponding value is updated. If inserted or updated, true is returned. 
+   // If not inserted, then false is returned.
+   public boolean insert(Object key, Object value) {
+      // Hash the key to get the bucket index
+      int bucketIndex = hash(key) % table.length;
+      
+      // Traverse the linked list, searching for the key. If the key exists in 
+      // an item, the value is replaced. Otherwise a new item is appended.
+      ChainingHashTableItem item = table[bucketIndex];
+      ChainingHashTableItem previous = null;
+      while (item != null) {
+         if (key.equals(item.key)) {
+            item.value = value;
+            return true;
+         }
+         
+         previous = item;
+         item = item.next;
+      }
+      
+      // Append to the linked list
+      if (table[bucketIndex] == null) {
+         table[bucketIndex] = new ChainingHashTableItem(key, value);
+      }
+      else {
+         previous.next = new ChainingHashTableItem(key, value);
+      }
+      return true;
+   }
+   
+   // Searches for the specified key. If found, the key/value pair is removed 
+   // from the hash table and true is returned. If not found, false is returned.
+   public boolean remove(Object key) {
+      // Hash the key to get the bucket index
+      int bucketIndex = hash(key) % table.length;
+      
+      // Search the bucket's linked list for the key
+      ChainingHashTableItem item = table[bucketIndex];
+      ChainingHashTableItem previous = null;
+      while (item != null) {
+         if (key.equals(item.key)) {
+            if (previous == null) {
+               // Remove linked list's first item
+               table[bucketIndex] = item.next;
+            }
+            else {
+               previous.next = item.next;
+            }
+            return true;
+         }
+         
+         previous = item;
+         item = item.next;
+      }
+      
+      return false; // key not found
+   }
+   
+   // Searches for the key, returning the corresponding value if found, null if 
+   // not found.
+   public Object search(Object key) {
+      // Hash the key to get the bucket index
+      int bucketIndex = hash(key) % table.length;
+      
+      // Search the bucket's linked list for the key
+      ChainingHashTableItem item = table[bucketIndex];
+      while (item != null) {
+         if (key.equals(item.key)) {
+            return item.value;
+         }
+         item = item.next;
+      }
+      
+      return null; // key not found
+   }
+}
