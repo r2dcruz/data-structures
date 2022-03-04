64a665c3542d77d68eafde65eb74129ea9d346e2
Romaine De La Cruz <r2dcruz@gmail.com>
2022-03-03 23:22:58 -0800

    algorithm

---
+++ algorithm/binarysearchtrees.java
@@ -0,0 +1,15 @@
+BSTSearch(tree, key) {
+   cur = tree⇢root
+   while (cur is not null) {
+      if (key == cur⇢key) {
+         return cur // Found
+      }
+      else if (key < cur⇢key) {
+         cur = cur⇢left
+      }
+      else {
+         cur = cur⇢right
+      }
+   }
+   return null // Not found
+}
