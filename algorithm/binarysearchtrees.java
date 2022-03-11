17d2acaea813561d0738b5fbf1cb969cf0e3fdab
Romaine De La Cruz <r2dcruz@gmail.com>
2022-03-10 23:46:51 -0800

    search demo

--- algorithm/binarysearchtrees.java
+++ algorithm/binarysearchtrees.java
@@ -105,3 +105,38 @@ BSTPrintInorder(node) {
    Print node                    // "Cur"
    BSTPrintInorder(node⇢right)  // "R"
 }
+
+import java.util.Scanner;
+
+public class BinarySearchTreeDemo {
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      BinarySearchTree tree = new BinarySearchTree();
+
+      // Ask user for values to insert
+      System.out.print("Enter values to insert with spaces between: ");
+      String userValues = scnr.nextLine();
+      System.out.println();
+
+      // Add each value to the tree
+      for (String value : userValues.split(" ")) {
+         int key = Integer.parseInt(value);
+         tree.insert(new Node(key));
+      }
+
+      // Show the tree
+      System.out.println("Initial tree:");
+      System.out.println(BSTPrint.treeToString(tree.getRoot()));
+      System.out.println();
+
+      // Ask the user for a value to remove
+      System.out.print("Enter value to remove: ");
+      String removeValueString = scnr.nextLine();
+      int removeValue = Integer.parseInt(removeValueString);
+      System.out.println();
+
+      System.out.printf("Tree after removing %d:%n", removeValue);
+      tree.remove(removeValue);
+      System.out.println(BSTPrint.treeToString(tree.getRoot()));
+   }
+}
