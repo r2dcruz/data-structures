18f165ffd6071488d9a09695ad822a91945f39f1
Romaine De La Cruz <r2dcruz@gmail.com>
2022-03-14 23:07:04 -0700

    graph practice

--- structures/graphs.java
+++ structures/graphs.java
@@ -49,3 +49,53 @@ public boolean hasEdge(Vertex fromVertex, Vertex toVertex) {
 
    return false;
 }
+
+class Graph {
+   // Maps a vertex to an ArrayList of all edges that start from that vertex
+   private HashMap<Vertex, ArrayList<Edge>> fromEdges;
+   
+   // Maps a vertex to an ArrayList of all edges that go to that vertex
+   private HashMap<Vertex, ArrayList<Edge>> toEdges;
+    
+   public Graph() {
+      fromEdges = new HashMap<Vertex, ArrayList<Edge>>();
+      toEdges = new HashMap<Vertex, ArrayList<Edge>>();
+   }
+
+   // Returns a collection of all edges in the graph
+   public Collection<Edge> getEdges() {
+      HashSet<Edge> edges = new HashSet<Edge>();
+      for (ArrayList<Edge> edgeList : fromEdges.values()) {
+         edges.addAll(edgeList);
+      }
+      return edges;
+   }
+
+   // Returns the collection of edges with the specified fromVertex
+   public Collection<Edge> getEdgesFrom(Vertex fromVertex) {
+      return fromEdges.get(fromVertex);
+   }
+
+   // Returns the collection of edges with the specified toVertex
+   public Collection<Edge> getEdgesTo(Vertex toVertex) {
+      return toEdges.get(toVertex);
+   }
+
+   // Returns a vertex with a matching label, or null if no such vertex exists
+   public Vertex getVertex(String vertexLabel) {
+      // Search the collection of vertices for a vertex with a matching label
+      for (Vertex vertex : getVertices()) {
+         if (vertex.label.equals(vertexLabel)) {
+            return vertex;
+         }
+      }
+      return null;
+   }
+
+   // Returns the collection of all of this graph's vertices
+   public Collection<Vertex> getVertices() {
+      return fromEdges.keySet();
+   }
+
+   ...
+}
