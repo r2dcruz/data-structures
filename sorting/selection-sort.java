55bc04196d3cf16fdd4c9f316a807fd6264d9698
Romaine De La Cruz <r2dcruz@gmail.com>
2022-03-30 06:09:14 -0700

    reviewing selection

--- sorting/selection-sort.java
+++ sorting/selection-sort.java
@@ -125,3 +125,23 @@ main() {
    }
    printLine()   
 }
+
+// reviewing
+
+for (i = 0; i < numbersSize - 1; ++i) {
+
+   // Find index of smallest remaining element
+   indexSmallest = i
+   for (j = i + 1; j < numbersSize; ++j) {
+
+      if (numbers[j] < numbers[indexSmallest]) {
+         indexSmallest = j
+      }
+   }
+
+   // Swap numbers[i] and numbers[indexSmallest]
+   temp = numbers[i]
+   numbers[i] = numbers[indexSmallest]
+   numbers[indexSmallest] = temp
+}
+
