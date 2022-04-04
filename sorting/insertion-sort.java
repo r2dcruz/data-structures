73ba1c2275470a73dd6f6889add41b37b7a93d4f
Romaine De La Cruz <r2dcruz@gmail.com>
2022-04-04 09:01:24 -0700

    insertion

--- sorting/insertion-sort.java
+++ sorting/insertion-sort.java
@@ -28,3 +28,45 @@ public class InsertionSortDemo {
       System.out.println("SORTED: " + Arrays.toString(numbers));
    }
 }
+
+// review
+
+InsertionSort(numbers, numbersSize) {
+   i = 0
+   j = 0
+   temp = 0  // Temporary variable for swap
+   
+   for (i = 1; i < numbersSize; ++i) {
+      j = i
+      // Insert numbers[i] into sorted part
+      // stopping once numbers[i] in correct position
+      while (j > 0 && numbers[j] < numbers[j - 1]) {
+         
+         // Swap numbers[j] and numbers[j - 1]
+         temp = numbers[j]
+         numbers[j] = numbers[j - 1]
+         numbers[j - 1] = temp
+         --j
+      }
+   }
+}
+
+main() {
+   numbers = { 10, 2, 78, 4, 45, 32, 7, 11 }
+   NUMBERS_SIZE = 8
+   i = 0
+   
+   print("UNSORTED: ")
+   for(i = 0; i < NUMBERS_SIZE; ++i) {
+      print(numbers[i] + " ")
+   }
+   printLine()
+   
+   InsertionSort(numbers, NUMBERS_SIZE)
+   
+   print("SORTED: ")
+   for(i = 0; i < NUMBERS_SIZE; ++i) {
+      print(numbers[i] + " ")
+   }
+   printLine()
+}
