2cfe5a3a9085e301cfe1849a5834d6e0eab3d3a6
Romaine De La Cruz <r2dcruz@gmail.com>
2022-04-07 23:43:42 -0700

    interleave insertion

--- sorting/shell-sort.java
+++ sorting/shell-sort.java
@@ -76,3 +76,18 @@ InsertionSortInterleaved(numbers, numbersSize, startIndex, gap) {
       }
    }
 }
+
+// interleaved insertion
+
+void insertionSortInterleaved(int[] numbers, int startIndex, int gap) {
+   for (int i = startIndex + gap; i < numbers.length; i += gap) {
+      int j = i;
+      while (j - gap >= startIndex && numbers[j] < numbers[j - gap]) {
+         // Swap numbers[j] and numbers [j - gap]
+         int temp = numbers[j];
+         numbers[j] = numbers[j - gap];
+         numbers[j - gap] = temp;
+         j -= gap;
+      }
+   }
+}
