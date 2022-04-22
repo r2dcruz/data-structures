1fc566e9c525fe2fb7ad543cc235ac124af11192
Romaine De La Cruz <r2dcruz@gmail.com>
2022-04-21 23:38:21 -0700

    non-negative

--- sorting/radix.java
+++ sorting/radix.java
@@ -98,3 +98,28 @@ public class RadixSortDemo {
    }
 }
 
+// non-negative integers
+
+
+RadixSort(array, arraySize) {
+   buckets = create array of 10 buckets
+
+   // Find the max length, in number of digits
+   maxDigits = RadixGetMaxLength(array, arraySize)
+      
+   // Start with the least significant digit
+   pow10 = 1
+   for (digitIndex = 0; digitIndex < maxDigits; digitIndex++) {
+      for (i = 0; i < arraySize; i++) {
+         bucketIndex = abs(array[i] / pow10) % 10
+         Append array[i] to buckets[bucketIndex]
+      }
+      arrayIndex = 0
+      for (i = 0; i < 10; i++) {
+         for (j = 0; j < buckets[i]⇢size(); j++)
+            array[arrayIndex++] = buckets[i][j]
+      }
+      pow10 = 10 * pow10
+      Clear all buckets
+   }
+}
