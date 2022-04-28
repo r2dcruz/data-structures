4a61bc48a1588ce4fe8e4bc112d385265428a98e
Romaine De La Cruz <r2dcruz@gmail.com>
2022-04-27 23:57:20 -0700

    java built in

--- sorting/insertion-sort.java
+++ sorting/insertion-sort.java
@@ -103,3 +103,25 @@ public class InsertionSortDemo {
       System.out.println("SORTED: " + Arrays.toString(numbers));
    }
 }
+
+// java built in sort, putting in here for now
+
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class JavaBuiltInSorting {
+   public static void main(String[] args) {
+      int[] numbers = { 3, 7, 2, 8, 12, 4, 9, 5 };
+      System.out.println("Unsorted array: " + Arrays.toString(numbers));
+      Arrays.sort(numbers);
+      System.out.println("Sorted array:   " + Arrays.toString(numbers));
+      
+      ArrayList<String> fruitList = new ArrayList<String>();
+      Collections.addAll(fruitList, "grape", "banana", "apple", "strawberry", "blueberry");
+      System.out.println("Unsorted list:  " + fruitList.toString());
+      Collections.sort(fruitList);
+      System.out.println("Sorted list:    " + fruitList.toString());
+   }
+}
