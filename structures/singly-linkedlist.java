286dded73a2211b1e5ecad31110d705c32f7f730
Romaine De La Cruz <r2dcruz@gmail.com>
2022-05-25 22:50:33 -0700

    doubly insertion

--- structures/singly-linkedlist.java
+++ structures/singly-linkedlist.java
@@ -248,3 +248,51 @@ ListFindInsertionPosition(list, dataValue) {
    }
    return curNodeA
 }
+
+// public void insertionSortDoublyLinked() {
+   Node currentNode = m_head.next;
+   while (currentNode != null) {
+      Node nextNode = currentNode.next;
+      Node searchNode = currentNode.previous;
+         
+      while (searchNode != null && searchNode.data > currentNode.data)
+         searchNode = searchNode.previous;
+      
+      // Remove and re-insert currentNode
+      remove(currentNode);
+      if (searchNode == null) {
+         currentNode.previous = null;
+         prepend(currentNode);
+      }
+      else {
+         insertAfter(searchNode, currentNode);
+      }
+
+      // Advance to next node
+      currentNode = nextNode;
+   }
+}
+
+public void insertionSortDoublyLinked() {
+   Node currentNode = m_head.next;
+   while (currentNode != null) {
+      Node nextNode = currentNode.next;
+      Node searchNode = currentNode.previous;
+         
+      while (searchNode != null && searchNode.data > currentNode.data)
+         searchNode = searchNode.previous;
+      
+      // Remove and re-insert currentNode
+      remove(currentNode);
+      if (searchNode == null) {
+         currentNode.previous = null;
+         prepend(currentNode);
+      }
+      else {
+         insertAfter(searchNode, currentNode);
+      }
+
+      // Advance to next node
+      currentNode = nextNode;
+   }
+}
