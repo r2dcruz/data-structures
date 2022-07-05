af07580443b72597d9154c273401199a94e2f366
Romaine De La Cruz <r2dcruz@gmail.com>
2022-07-04 22:08:30 -0700

    arraylist

--- structures/singly-linkedlist.java
+++ structures/singly-linkedlist.java
@@ -805,3 +805,21 @@ ArrayListRemoveAt(list, index) {
       list⇢length = list⇢length - 1
    }
 }  
+
+// arraylist
+
+class ArrayList {
+   // Private members
+   private int[] arrayData;
+   private int arrayListLength;
+
+   // The default constructor initializes the list with a capacity of 4
+   public ArrayList() {
+      this(4);
+   }
+   
+   public ArrayList(int capacity) {
+      this.arrayData = new int[capacity];
+      this.arrayListLength = 0;
+   }
+}
