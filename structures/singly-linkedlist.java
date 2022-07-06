55e792489a40d845e396ab6adfc00c69a11ac103
Romaine De La Cruz <r2dcruz@gmail.com>
2022-07-05 23:33:48 -0700

    arraylisttt

--- structures/singly-linkedlist.java
+++ structures/singly-linkedlist.java
@@ -823,3 +823,31 @@ class ArrayList {
       this.arrayListLength = 0;
    }
 }
+
+// . 
+
+public void append(int newItem) {
+   // Resize if the array is full
+   if (arrayData.length == arrayListLength) {
+      resize(arrayListLength * 2);
+   }
+
+   // Insert the new item at index arrayListLength
+   arrayData[arrayListLength] = newItem;
+
+   // Increment the array's length
+   ++arrayListLength;
+}
+   
+public void resize(int newAllocationSize) {
+   // Create a new array with the indicated size
+   int[] newArray = new int[newAllocationSize];
+
+   // Copy items in current array into the new array
+   for (int i = 0; i < arrayListLength; ++i) {
+      newArray[i] = arrayData[i];
+   }
+
+   // Assign the arrayData member with the new array
+   arrayData = newArray;
+}
