b3f74a5f5a3de71a3cb529f1b5c7dfba58483745
Romaine De La Cruz <r2dcruz@gmail.com>
2022-11-08 14:25:25 -0800

    review

--- algorithm/binarysearchtrees.java
+++ algorithm/binarysearchtrees.java
@@ -163,3 +163,44 @@ TrieInsert(trieRoot, "APPLE")
 TrieInsert(trieRoot, "APPLY")
 TrieInsert(trieRoot, "APP")
 >>>>>>> 326a3f54f40b1749f2b96c6799d5ba5ee5b55655
+
+
+// search
+
+LinearSearch(numbers, numbersSize, key) {
+   i = 0
+
+   for (i = 0; i < numbersSize; ++i) {
+      if (numbers[i] == key) {
+         return i
+      }
+   }
+      
+   return -1 // not found
+}
+   
+main() {
+   numbers = {2, 4, 7, 10, 11, 32, 45, 87}
+   NUMBERS_SIZE = 8
+   i = 0
+   key = 0
+   keyIndex = 0
+      
+   print("NUMBERS: ")
+   for (i = 0; i < NUMBERS_SIZE; ++i) {
+      print(numbers[i] + " ")
+   }
+   printLine()
+      
+   print("Enter a value: ")
+   key = getIntFromUser()
+      
+   keyIndex = LinearSearch(numbers, NUMBERS_SIZE, key)
+      
+   if (keyIndex == -1) {
+      printLine(key + " was not found.")
+   } 
+   else {
+      printLine("Found " + key + " at index " + keyIndex + ".")
+   }
+}
